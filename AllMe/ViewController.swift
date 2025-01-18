7014949280459315c885538f73ec66a4c6f5bb1f
89Explorer <leral3@naver.com>
2025-01-18 19:55:48 +0900

    Initial Commit

---
+++ AllMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllMe
+//
+//  Created by 권정근 on 1/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AllMeTests/AllMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllMeTests.swift
+//  AllMeTests
+//
+//  Created by 권정근 on 1/18/25.
+//
+
+import Testing
+@testable import AllMe
+
+struct AllMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
